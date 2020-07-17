6bd9c7b45919ca46d752a8a3677d5a085c7851c2
victorpena13 <victor.pena1392@gmail.com>
2020-07-16 20:39:48 -0500

    first hello world in c++

---
+++ HelloWorld/HelloWorld/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Victor Pena on 7/16/20.
+//  Copyright © 2020 Victor Pena. All rights reserved.
+//
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
